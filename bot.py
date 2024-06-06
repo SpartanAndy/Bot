7ebc3d65a8161d8cd862818a0c2efd76e8bb56b6
SpartanAndy <andy-2610@outlook.com>
2024-06-06 12:16:37 -0600

    bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,11 @@
+import random
 import discord
 from discord.ext import commands
+import os
+import requests
+
+
+m = random.randint
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -23,5 +29,28 @@ async def repeat(ctx, times: int, content='repeating...'):
     """Repeats a message multiple times."""
     for i in range(times):
         await ctx.send(content)
-        
+@bot.command()
+
+async def mem(ctx):
+    imagen = random.choice(os.listdir('images'))
+    with open(f'images/{imagen}','rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+    
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+
 bot.run("Token")
+
